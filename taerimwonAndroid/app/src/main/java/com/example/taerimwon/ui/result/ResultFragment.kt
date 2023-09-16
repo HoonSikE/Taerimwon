f82a77f4ae5a7d60d5aea88992bdd5880b745346
HanHoon <hoongrammer@gmail.com>
2023-09-16 22:36:15 +0900

    Update

--- taerimwonAndroid/app/src/main/java/com/example/taerimwon/ui/result/ResultFragment.kt
+++ taerimwonAndroid/app/src/main/java/com/example/taerimwon/ui/result/ResultFragment.kt
@@ -27,44 +27,8 @@ class ResultFragment : BaseFragment<FragmentResultBinding>(R.layout.fragment_res
         }
         binding.buttonMMS.setOnClickListener{
             // MMS 보내기
-            // 수신자 전화번호
-            val recipientNumber = "01000000000"
-            // MMS 제목
-            val mmsSubject = "MMS 제목"
-            // MMS 내용
-            val mmsMessage = "MMS 내용"
-            // 파일 URI, 예: content://media/external/images/media/1
-            val imageUri = Uri.parse("content://media/external/images/media/1")
-            sendMMS(recipientNumber, mmsSubject, mmsMessage, imageUri)
         }
     }
     private fun observer() {
-    }
-
-    // MMS 메시지 보내기 함수
-    fun sendMMS(recipient: String, subject: String, message: String, imageUri: Uri) {
-        // MMS 전송을 위한 데이터 준비
-        val values = ContentValues()
-        values.put(Telephony.Mms.ADDR, recipient) // 수신자 전화번호
-        values.put(Telephony.Mms.SUBJECT, subject) // MMS 제목
-        values.put(Telephony.Mms.TEXT_ONLY, message) // MMS 내용
-
-        // MMS 메시지를 MMS 데이터베이스에 추가
-        val contentResolver = applicationContext.contentResolver
-        val mmsUri = contentResolver.insert(Telephony.Mms.Outbox.CONTENT_URI, values)
-
-        // MMS 이미지 추가
-        val imageValues = ContentValues()
-        imageValues.put(Telephony.Mms.Part.MSG_ID, mmsUri?.lastPathSegment)
-        imageValues.put(Telephony.Mms.Part.CONTENT_TYPE, "image/jpeg")
-        imageValues.put(Telephony.Mms.Part.NAME, "image.jpg")
-        imageValues.put(Telephony.Mms.Part.FILE_NAME, "image.jpg")
-        imageValues.put(Telephony.Mms.Part.URI, imageUri.toString())
-
-        contentResolver.insert(Telephony.Mms.Part.CONTENT_URI, imageValues)
-
-        // MMS 메시지 전송
-        val smsManager = SmsManager.getDefault()
-        smsManager.sendMultimediaMessage(applicationContext, mmsUri)
     }
 }
